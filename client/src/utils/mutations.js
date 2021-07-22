f8c35daaae3e1457b10d5018713966d6360ec5fe
desantel <ecdesantis21@gmail.com>
2021-07-21 21:09:57 -0400

    mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -8,6 +8,51 @@ export const ADD_USER = gql`
                 email: $email,
                 password: $password) {
                     token
+                    use {
+                        _id
+                        username
+                    }
                 }
         }
-`
+`;
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!,
+        $password: String!) {
+            login(email: $email, 
+                password: $password) {
+                token
+                user {
+                    _id
+                    username
+                }
+            }
+        }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: savedBook!) {
+        saveBook(book: $book) {
+            username
+            savedBooks {
+                bookId
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookID: $bookId) {
+            username
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+            }
+        }
+    }
+`;
